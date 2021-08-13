2432ecd314f3c55a1882b9f434b3a24c3211323b
Rupert Foggo McKay <maxfmckay@gmail.com>
2021-08-13 15:05:58 +0200

    Cube builder

--- src/index.ts
+++ src/index.ts
@@ -7,27 +7,42 @@ import {
   WebGLRenderer,
 } from "three";
 
+const buildCube = () => {
+  const depth = 1 + Math.random() * 10;
+  const xOffset = Math.random() * 4;
+  const spinSpeed = 0.005 + Math.random() * 0.01;
+  const mesh = new Mesh(
+    new BoxGeometry(0.2, 0.2, 0.2),
+    new MeshNormalMaterial()
+  );
+  mesh.position.z = -depth;
+  mesh.position.y = -0.5;
+  const update = (time: number) => {
+    mesh.rotation.x += spinSpeed;
+    mesh.rotation.y += spinSpeed;
+    mesh.position.x = 2 - ((time / 1000 + xOffset) % 4);
+  };
+  return { mesh, update };
+};
+
+const buildCubes = (count: number) => new Array(count).fill(0).map(buildCube);
+
 const camera = new PerspectiveCamera(
   70,
   window.innerWidth / window.innerHeight,
   0.01,
-  10
+  1000
 );
-camera.position.z = 1;
 
 const scene = new Scene();
 
-const geometry = new BoxGeometry(0.2, 0.2, 0.2);
-const material = new MeshNormalMaterial();
-
-const mesh = new Mesh(geometry, material);
-scene.add(mesh);
+const cubes = buildCubes(30);
+cubes.forEach((cube) => scene.add(cube.mesh));
 
 const renderer = new WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop((time) => {
-  mesh.rotation.x = time / 2000;
-  mesh.rotation.y = time / 1000;
+  cubes.forEach((cube) => cube.update(time));
 
   renderer.render(scene, camera);
 });
